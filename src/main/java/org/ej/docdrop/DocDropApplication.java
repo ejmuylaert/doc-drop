6540b6b22988f81027199d641f4a47d0d64ee545
Ernst-Jan Muylaert <ernst-jan.muylaert@wearewuunder.com>
2021-04-03 10:45:32 +0200

    Create project from start.spring.io

---
+++ src/main/java/org/ej/docdrop/DocDropApplication.java
@@ -0,0 +1,13 @@
+package org.ej.docdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocDropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ej/docdrop/DocDropApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ej.docdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocDropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
